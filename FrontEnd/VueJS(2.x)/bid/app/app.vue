a7fd37680fd090db87b4a75a2c6090afdc600d6e
ff4c00 <ff4c00@gmail.com>
2018-10-19 00:30:09 +0800

    竞价需求填写页面步骤一

--- FrontEnd/VueJS(2.x)/bid/app/app.vue
+++ FrontEnd/VueJS(2.x)/bid/app/app.vue
@@ -1,17 +1,119 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="root">
+  
+  <div class="root-topbar">    
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1">可点链接</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">下拉菜单</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>不可点链接</el-menu-item>
+      <el-menu-item index="4"><a href="" target="_blank">后台管理</a></el-menu-item>
+    </el-menu>
+  </div>
+
+  <div class="root-sidebar">
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">收起</el-radio-button>
+    </el-radio-group>
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item-group>
+          <span slot="title">分组一</span>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <span slot="title">选项4</span>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+
+  <div class="root-main-view">
+    <router-view ></router-view>    
+  </div>
+
+
   </div>
 </template>
 
 <script>
-  export default {
-    computed: {
-      
+export default {
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1',
+      isCollapse: true
     }
+  },
+  methods: {
+
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  },
+  computed: {
+    
   }
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+
+.root {
+  &-sidebar {
+    width: 200px;
+    float: left;
+  }
+
+  &-main-view {
+    margin: 32px;
+    float: left;
+    // height: 100%;
+    height: 500px;
+  }
+
+}
 </style>
--- FrontEnd/VueJS(2.x)/bid/app/views/projects/new.vue
+++ FrontEnd/VueJS(2.x)/bid/app/views/projects/new.vue
@@ -1,15 +1,84 @@
 <template>
   <div class="new">
-    <div class="new-base">
+    <div class="new-base" style="width: 500px;">
 
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form" :model="form" label-width="150px" @submit.native.prevent label-position="left">
       
         <el-form-item label="项目名称">
-          <el-input placeholder="请输入项目名称" v-model="form.name" clearable></el-input>
+          <el-input placeholder="请输入项目名称" v-model="form.project_name" clearable></el-input>
         </el-form-item>
       
         <el-form-item label="预算金额">
-          <el-input-number v-model="form.total" :precision="2" :step="0.1" :max="10000"></el-input-number>
+          <el-input-number v-model="form.budget_amount" :precision="2" :step="1" :max="10000"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="采购单位名称">
+          <el-input placeholder="请输入采购单位名称" v-model="form.department_name" clearable></el-input>
+        </el-form-item>
+
+        <el-form-item label="挂网天数">
+          <el-input-number v-model="form.online_days" :precision="0" :step="1" :max="7"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="联系人姓名">
+          <el-input placeholder="请输入联系人姓名" v-model="form.contact_name" clearable></el-input>
+        </el-form-item>
+
+        <el-form-item label="是否公开联系方式">
+          <el-switch v-model="form.show_contact" active-text="是" inactive-text="否"> </el-switch>
+        </el-form-item>
+
+
+        <el-form-item label="支付方式">
+          <el-radio-group v-model="form.payment_method">
+            <el-radio-button v-for="payment in form.payment_methods" :label="payment.name" :key='payment.id'></el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="发票信息">
+          <el-select v-model="form.invoice_id" placeholder="请选择发票">
+            <el-option v-for="item in form.invoice_info" :key="item.id" :label="item.title" :value="item.id">
+              <span style="float: left">{{ item.title }}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">{{ item.type }}</span>
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="合同签订时间">
+          <el-input-number v-model="form.contract_time.value" :precision="form.contract_time.precision" :step="form.contract_time.step" :max="form.contract_time.max"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="交货时间">
+          <el-input-number v-model="form.delivery_time.value" :precision="form.contract_time.precision" :step="form.contract_time.step" :max="form.contract_time.max"></el-input-number>          
+        </el-form-item>
+
+        <el-form-item label="送货地址">
+          <el-select v-model="form.delivery_address" placeholder="请选择">
+            <el-option-group v-for="group in form.delivery_addresses" :key="group.label" :label="group.label">
+              <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-option-group>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="供应商资质要求">
+          <el-input type="textarea" autosize placeholder="请输入内容" v-model="form.father_requirement"> </el-input>
+        </el-form-item>
+
+        <el-form-item label="售后服务要求">
+          <el-input type="textarea" autosize placeholder="请输入内容" v-model="form.after_sale_service"> </el-input>
+        </el-form-item>
+
+        <el-form-item label="售后服务要求">
+          <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-change="handleChange" :file-list="form.fileList3">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div> -->
+          </el-upload>        
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button>取消</el-button>
         </el-form-item>
 
       </el-form>
@@ -24,9 +93,58 @@ export default {
     return {
       input10: '',
       form: {
-        name: '',
-        total: 0
+        project_name: '项目名称',
+        budget_amount: 0, // 预算总额
+        department_name: '采购单位名称',
+        online_days:5, //挂网天数
+        contact_name: '联系人姓名',
+        contact_tel: '联系人电话',
+        show_contact: false,// 是否公开联系方式
+        payment_methods: [{id: 1, name:'货到付款'}, {id: 2, name: '帐期支付'}], //'支付方式'
+        payment_method: '货到付款',
+        invoice_info:[{id: 1, title: '发票抬头一', type: '增值税普通发票'}, {id: 2, title: '发票抬头二', type: '增值税专用发票'}],
+        invoice_id: 1,
+        contract_time: {precision: 0, step: 1, max: 7, value: 3},// 成交后n日内签订合同
+        delivery_time: {precision: 0, step: 1, max: 7, value: 3},// 合同签订后n日内交货
+        delivery_addresses: [{
+          label: '常用地址',
+          options: [{
+            value: 1,
+            label: '常用地址一'
+          }, {
+            value: 2,
+            label: '常用地址二'
+          }]
+        }, {
+          label: '其他地址',
+          options: [{
+            value: 3,
+            label: '其他地址一'
+          }, {
+            value: 4,
+            label: '其他地址二'
+          }]
+        }],
+        delivery_address: null,
+        father_requirement: '',// 供应商资质要求
+        after_sale_service: '',//售后服务
+        attachment: '', // 附件
+        fileList3: [{
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }, {
+          name: 'food2.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }]
       }
+    }
+  },
+  methods: {
+    handleChange(file, fileList) {
+      this.fileList3 = fileList.slice(-3);
+    },
+    onSubmit() {
+      console.log('submit!');
     }
   }
 }
@@ -35,15 +153,15 @@ export default {
 <style lang="scss" scoped>
 .new {
   &-base {
-    .el-row {
-      width: 100%;
-    }
-    span {
-      float: left;
-    }
-    div {
-      float: left;
-    }
+    // .el-row {
+    //   width: 100%;
+    // }
+    // span {
+    //   float: left;
+    // }
+    // div {
+    //   float: left;
+    // }
 
   }
 }
