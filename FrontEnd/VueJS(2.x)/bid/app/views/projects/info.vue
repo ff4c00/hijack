af93c607d08e66ed8d5a8b678771642077633ce6
ff4c00 <ff4c00@gmail.com>
2018-10-24 23:19:29 +0800

    列表详情

---
+++ FrontEnd/VueJS(2.x)/bid/app/views/projects/info.vue
@@ -0,0 +1,34 @@
+<template slot-scope="props">
+  <el-form label-position="left" inline class="index-table-expand">
+    <el-form-item label="项目名称">
+      <span>{{ project.name }}</span>
+    </el-form-item>
+    <el-form-item label="申购单位">
+      <span>{{ project.department_name }}</span>
+    </el-form-item>
+    <el-form-item label="预算金额">
+      <span>{{ project.budget_amount }}</span>
+    </el-form-item>
+    <el-form-item label="电话">
+      <span>{{ project.contact_tel }}</span>
+    </el-form-item>
+    <el-form-item label="送货地址">
+      <span>{{ project.delivery_address }}</span>
+    </el-form-item>
+    <el-form-item label="供应商资质要求">
+      <span>{{ project.father_requirement }}</span>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  props: {
+    project: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
